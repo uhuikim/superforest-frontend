2cb34ff016ed520e7163b30759335c22bcb9b757
leye195 <leye195@naver.com>
2020-12-30 10:37:53 +0900

    feat: Apollo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,10 @@ const Pstyle = css`
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo} />
-        <p css={Pstyle}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p css={Pstyle}>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
     </div>
   );
 };
