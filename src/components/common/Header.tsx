0999b9491dad89e16dd77e59c8ea2279ebd2005f
leye195 <leye195@naver.com>
2021-01-25 08:49:09 +0900

    feat: tailwind setting

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import tw from 'twin.macro';
+
+const headerStyle = tw`
+h-20 bg-green-100
+`;
 
 const Header: React.FC = () => {
-  return <header></header>;
+  return <header css={headerStyle}></header>;
 };
 
 export default Header;
