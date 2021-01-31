105efc08dc5253c2dd91044a8b10017ab9d9fc95
uhui <uhui94@naver.com>
2021-01-31 16:33:02 +0900

    feat:login

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Form: React.FC = () => {
+  return <div>djdjdj</div>;
+};
+
+export default Form;
--- src/pages/Home/HomePresentation.tsx
+++ src/pages/Home/HomePresentation.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Form from 'components/Form/Form';
+
+import tw from 'twin.macro';
+
+const container = tw`
+container 
+mx-auto
+`;
 
 const HomePresentation = () => {
-  return <div>Home</div>;
+  return (
+    <div css={container}>
+      <Form />
+    </div>
+  );
 };
 
 export default HomePresentation;
